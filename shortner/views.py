4a7ae07017ae19656a71d781e77af33fff7b7284
Kush Kothari <kushkothari2001@gmail.com>
2021-03-25 00:33:55 +0530

    Added basic way to route for database object creation

--- shortner/views.py
+++ shortner/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+import uuid
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        url = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
